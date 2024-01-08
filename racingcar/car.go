564a134188601359ce5c1330e9419b344a6714b3
echo724 <echo.backend@gmail.com>
2024-01-08 16:50:57 +0900

    feat: 자동차 생성 검증 로직 구현

--- racingcar/car.go
+++ racingcar/car.go
@@ -3,17 +3,17 @@ package racingcar
 import "errors"
 
 type Car struct {
-	name     string
-	position int
+	Name     string
+	Position int
 }
 
 func NewCar(name string) (*Car, error) {
 	if !checkValidCarName(name) {
 		return nil, errors.New("차 이름은 1자 이상 5자 이하만 가능합니다")
 	}
-	return &Car{name: name, position: 0}, nil
+	return &Car{Name: name, Position: 0}, nil
 }
 
 func checkValidCarName(name string) bool {
-	return 0 < len(name) || len(name) <= 5
+	return 0 < len(name) && len(name) <= 5
 }
