2c30ea7f7fa711490747dc87ac420df2625c62a4
thedevluffy <thedevluffy@gmail.com>
2024-01-13 23:27:37 +0900

    🧪 테스트 추가

---
+++ test/racingcar_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"github.com/stretchr/testify/assert"
+	. "github.com/thedevluffy/go-racing-car/racingcar"
+	"testing"
+)
+
+func TestMovableCarMoveForward(t *testing.T) {
+	car := Car{Name: "개발자동차", Position: 0}
+
+	movedCar := car.MoveForward()
+
+	assert.Equal(
+		t,
+		car.Position+1,
+		movedCar.Position,
+		"Expected car position to be %d, but got %d", car.Position+1, movedCar.Position,
+	)
+}
+
+func TestMovableCarStop(t *testing.T) {
+	car := Car{Name: "개발자동차", Position: 0}
+
+	stoppedCar := car.Stop()
+
+	assert.Equal(
+		t,
+		car.Position,
+		stoppedCar.Position,
+		"Expected car position to be %d, but got %d", car.Position+1, stoppedCar.Position,
+	)
+}
