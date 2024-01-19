d833a70399451551fd53a6dad3c72fe250da54f6
echo724 <echo.backend@gmail.com>
2024-01-19 14:52:22 +0900

    feat: 게임 실행 및 우승자 이름 구하는 로직 구현

--- racingcar/game.go
+++ racingcar/game.go
@@ -12,3 +12,23 @@ func NewGame(carNames []string, fuelGenerator FuelGenerator) (*Game, error) {
 	}
 	return &Game{Cars: cars, FuelGenerator: fuelGenerator}, nil
 }
+
+func (g *Game) Run() (*RunResult, error) {
+	for _, car := range g.Cars.Cars {
+		fuel, _ := g.FuelGenerator.Fuel()
+		err := car.Move(fuel)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return GenerateRunResult(g.Cars), nil
+}
+
+func (g *Game) GetWinnersName() []string {
+	winners := g.Cars.GetWinners()
+	winnersName := make([]string, len(winners))
+	for i, winner := range winners {
+		winnersName[i] = winner.Name
+	}
+	return winnersName
+}
