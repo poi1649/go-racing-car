11c18bded86ce064cc899601adae104e47102575
echo724 <echo.backend@gmail.com>
2024-01-19 14:29:14 +0900

    test: 고정연료 제공자 테스트 구현

---
+++ test/fuel_generator_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"github.com/poi1649/go-racing-car/racingcar"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewFuelGenerator(t *testing.T) {
+	fuels := []int{1, 2, 3}
+	generator := racingcar.NewFixedFuelGenerator(fuels)
+	fuel, _ := generator.Fuel()
+	assert.Equal(t, 1, fuel)
+}
+
+func TestNewFuelGeneratorWithOverLengthFuel(t *testing.T) {
+	fuels := []int{1, 2, 3}
+	generator := racingcar.NewFixedFuelGenerator(fuels)
+	generator.Fuel()
+	generator.Fuel()
+	generator.Fuel()
+	_, err := generator.Fuel()
+	assert.Equal(t, "제공된 연료 길이보다 많은 연료를 요청했습니다", err.Error())
+}
