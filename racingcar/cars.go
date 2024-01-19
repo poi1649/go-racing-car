c2b28a1f0f2f763f7fa2619914f94c844c5fceff
echo724 <echo.backend@gmail.com>
2024-01-19 13:39:08 +0900

    feat: 자동차 이름 배열로 자동차들을 생성하는 로직 구현

--- racingcar/cars.go
+++ racingcar/cars.go
@@ -7,3 +7,31 @@ type Cars struct {
 func NewCars(cars []*Car) *Cars {
 	return &Cars{Cars: cars}
 }
+
+func GenerateCars(names []string) (*Cars, error) {
+	cars := make([]*Car, len(names))
+	for i, name := range names {
+		car, err := NewCar(name)
+		if err != nil {
+			return nil, err
+		}
+		cars[i] = car
+	}
+	return NewCars(cars), nil
+}
+
+func (c *Cars) GetWinners() []*Car {
+	winners := make([]*Car, 0)
+	maxPosition := 0
+	for _, car := range c.Cars {
+		if car.Position > maxPosition {
+			winners = []*Car{car}
+			maxPosition = car.Position
+			continue
+		}
+		if car.Position == maxPosition {
+			winners = append(winners, car)
+		}
+	}
+	return winners
+}
