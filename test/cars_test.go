18a40b1dc680f8735555b1c15e281fc3496f7587
echo724 <echo.backend@gmail.com>
2024-01-19 13:41:04 +0900

    test: 자동차 집합에서 승리자 목록을 구하는 로직 테스트

--- test/cars_test.go
+++ test/cars_test.go
@@ -24,3 +24,30 @@ func TestGenerateCarsWithInvalidName(t *testing.T) {
 	errMsg := "차 이름은 1자 이상 5자 이하만 가능합니다"
 	assert.Equal(t, errMsg, err.Error())
 }
+
+func TestGetWinners(t *testing.T) {
+	car1, _ := racingcar.NewCar("test1")
+	car2, _ := racingcar.NewCar("test2")
+	car3, _ := racingcar.NewCar("test3")
+	cars := racingcar.NewCars([]*racingcar.Car{car1, car2, car3})
+	car1.Move(3)
+	car2.Move(5)
+	car3.Move(1)
+	winners := cars.GetWinners()
+	assert.Equal(t, 1, len(winners))
+	assert.Equal(t, "test2", winners[0].Name)
+}
+
+func TestGetWinnersWithSamePosition(t *testing.T) {
+	car1, _ := racingcar.NewCar("test1")
+	car2, _ := racingcar.NewCar("test2")
+	car3, _ := racingcar.NewCar("test3")
+	cars := racingcar.NewCars([]*racingcar.Car{car1, car2, car3})
+	car1.Move(3)
+	car2.Move(5)
+	car3.Move(5)
+	winners := cars.GetWinners()
+	assert.Equal(t, 2, len(winners))
+	assert.Equal(t, "test2", winners[0].Name)
+	assert.Equal(t, "test3", winners[1].Name)
+}
