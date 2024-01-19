08b286afddd1e906a31fb2dc1a8ff52a66020f29
echo724 <echo.backend@gmail.com>
2024-01-19 14:52:15 +0900

    test: 게임 실행 및 우승자 이름 구하는 로직 테스트

--- test/game_test.go
+++ test/game_test.go
@@ -10,3 +10,21 @@ func TestNewGame(t *testing.T) {
 	game, _ := racingcar.NewGame([]string{"test1", "test2"}, racingcar.NewFixedFuelGenerator([]int{1, 2, 3}))
 	assert.Equal(t, game.Cars.Cars[0].Name, "test1")
 }
+
+func TestGameRun(t *testing.T) {
+	game, _ := racingcar.NewGame([]string{"test1", "test2"}, racingcar.NewFixedFuelGenerator([]int{1, 4}))
+	result, _ := game.Run()
+	assert.Equal(t, result.CarNameToPosition["test1"], 0)
+}
+
+func TestGameGetWinnersName(t *testing.T) {
+	game, _ := racingcar.NewGame([]string{"test1", "test2"}, racingcar.NewFixedFuelGenerator([]int{1, 4}))
+	game.Run()
+	assert.Equal(t, []string{"test2"}, game.GetWinnersName())
+}
+
+func TestGameGetWinnersNameWithSamePosition(t *testing.T) {
+	game, _ := racingcar.NewGame([]string{"test1", "test2"}, racingcar.NewFixedFuelGenerator([]int{4, 4}))
+	game.Run()
+	assert.Equal(t, []string{"test1", "test2"}, game.GetWinnersName())
+}
